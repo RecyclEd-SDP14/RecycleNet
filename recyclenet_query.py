2e8a9e97175ae43a58cfc688676d8fbfa6ce76f7
Oliver Smith <01smito01@gmail.com>
2022-02-22 12:09:56 +0000

    Removed leading 'b' from responses

--- recyclenet_query.py
+++ recyclenet_query.py
@@ -4,7 +4,7 @@ import requests
 def classify(filename):
     with open(filename, 'rb') as f:
         r = requests.post('http://34.79.127.126:5000/foo', files={'file':f})
-        print(r.content)
+        print(r.content.decode('UTF-8'))
 
 if __name__ == '__main__':
     classify(sys.argv[1])
