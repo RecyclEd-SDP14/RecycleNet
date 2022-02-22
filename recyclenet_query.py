5d5edaf466c88dcceb202a8a1e18da69e6fdd68b
Oliver Smith <01smito01@gmail.com>
2022-02-22 10:21:19 +0000

    recyclenet_query points to remote server

--- recyclenet_query.py
+++ recyclenet_query.py
@@ -3,7 +3,7 @@ import requests
 
 def classify(filename):
     with open(filename, 'rb') as f:
-        r = requests.post('http://127.0.0.1:5000/foo', files={'file':f})
+        r = requests.post('http://34.79.127.126:5000/foo', files={'file':f})
         print(r.content)
 
 if __name__ == '__main__':
